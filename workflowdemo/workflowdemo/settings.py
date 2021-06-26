28a474f386396abdd904336ea8b49d7d5ae02dc8
liroding <liroding@zhaoxin.com>
2021-06-26 15:02:34 +0800

    add logger debug

--- workflowdemo/workflowdemo/settings.py
+++ workflowdemo/workflowdemo/settings.py
@@ -167,3 +167,76 @@ WORKFLOWBACKENDURL = "http://127.0.0.1:10000"
 WORKFLOWTOKEN = "3c0fd000-d107-11eb-ae3f-0242ac110002"
 WORKFLOWAPPNAME = "ops"
 sys.path.insert(0,os.path.join(BASE_DIR,'Apps'))
+
+
+#logging日志配置
+#LOGGING = {
+# 'version': 1,
+# 'disable_existing_loggers': True,
+# 'formatters': {#日志格式 
+# 'standard': {
+#  'format': '%(asctime)s [%(threadName)s:%(thread)d] [%(name)s:%(lineno)d] [%(module)s:%(funcName)s] [%(levelname)s]- %(message)s'}, 
+# 'standard_A': {
+#  'format': '%(asctime)s  [%(name)s:%(lineno)d] [%(module)s:%(funcName)s] [%(levelname)s]- %(message)s'} 
+# },
+# 'filters': {#过滤器
+# 'require_debug_false': {
+#  '()': 'django.utils.log.RequireDebugFalse',
+#  }
+# },
+# 'handlers': {#处理器
+# 'null': {
+#  'level': 'DEBUG',
+#  'class': 'logging.NullHandler',
+# },
+# 'mail_admins': {#发送邮件通知管理员
+#  'level': 'ERROR',
+#  'class': 'django.utils.log.AdminEmailHandler',
+#  'filters': ['require_debug_false'],# 仅当 DEBUG = False 时才发送邮件
+#  'include_html': True,
+# },
+# 'debug': {#记录到日志文件(需要创建对应的目录，否则会出错)
+#  'level':'DEBUG',
+#  'class':'logging.handlers.RotatingFileHandler',
+#  'filename': os.path.join(BASE_DIR, "log",'debug.log'),#日志输出文件
+#  'maxBytes':1024*1024*5,#文件大小 
+#  'backupCount': 5,#备份份数
+#  'formatter':'standard_A',#使用哪种formatters日志格式
+# },
+#
+# 'console':{#输出到控制台
+#  'level': 'DEBUG',
+#  'class': 'logging.StreamHandler',
+#  'formatter': 'standard',
+# },
+# },
+# 'loggers': {#logging管理器
+# 'django': {
+#  'handlers': ['debug'],
+#  'level': 'DEBUG',
+#  'propagate': True,
+# },
+#
+#'django.db.backends': {
+#  'handlers': ['debug'],
+#  'propagate': True,
+#  'level':'ERROR',
+#},
+# 'django.request': {
+#  'handlers': ['debug','mail_admins'],
+#  'level': 'ERROR',
+#  'propagate': True,
+# },
+# # 对于不在 ALLOWED_HOSTS 中的请求不发送报错邮件
+# 'django.security.DisallowedHost': {
+#  'handlers': ['null'],
+#  'propagate': False,
+# },
+#
+# 'django.template':{
+#   'handlers':['null'],
+#   'level':'INFO',
+#   'propagate':True,
+# },
+# } 
+#}
